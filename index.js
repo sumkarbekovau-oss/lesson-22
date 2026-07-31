7bafbe53df65da157f4006198fce245d2079507c
Uulbolsun <sumkarbekovau@gmail.com>
2026-07-31 17:10:52 +0300

    new fix

---
+++ index.js
@@ -0,0 +1,6 @@
+const content = document.querySelector(".content")
+const computer = document.querySelector(".computer")
+const body = document.querySelector(".body")
+
+content.innerHTML = "Hello"
+computer.innerHTML = "World"
