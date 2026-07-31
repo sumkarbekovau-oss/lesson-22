09724eaf792ee12b226290d8a0b92eac87b0a110
Uulbolsun <sumkarbekovau@gmail.com>
2026-07-31 17:40:48 +0300

    new fix

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 const content = document.querySelector(".content")
+
+
 const computer = document.querySelector(".computer")
 const body = document.querySelector(".body")
 
 content.innerHTML = "Hello"
+// `<div class="picture>
+// <img src ="${pic.image}"/>
+// </div>`
 computer.innerHTML = "World"
+
+
