8203893e3a25f210fe38e24875434acea1917dd7
Uulbolsun <sumkarbekovau@gmail.com>
2026-08-03 11:54:38 +0300

    new fix

--- index.js
+++ index.js
@@ -5,9 +5,6 @@ const computer = document.querySelector(".computer")
 const body = document.querySelector(".body")
 
 content.innerHTML = "Hello"
-// `<div class="picture>
-// <img src ="${pic.image}"/>
-// </div>`
 computer.innerHTML = "World"
 
 
